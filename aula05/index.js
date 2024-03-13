1879a482323eb469c2157293bd59374432cea964
Marcos Altec <marcospassos2211@gmail.com>
2024-03-12 22:42:15 -0300

    Subindo arquivos Aula05 e Pratica4

--- aula05/index.js
+++ aula05/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const routerProdutos = require("./router");
 const app = express();
-
+app.use(express.json())
 app.use(routerProdutos);
 
 app.listen(3000, function(){
--- aula05/router.js
+++ aula05/router.js
@@ -2,18 +2,54 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/produtos", function(req, res) {
-    res.json([])
+const produtos = [{id: 1, nome: "Uva", preco: 10.00}]
+
+router.get("/produtos", function(req, res){
+  res.json(produtos);
 });
 
 router.get("/produtos/:produtoId", function(req, res){
-    if (req.params.produtoId == 10000) {
-        res.status(404).json({msg: "Produto não encontrado"});
-        return;
-    }
-    res.json({});
+    const encontrado = produtos.find((produto) => produto.id == req.params.produtoId);
+  if (!encontrado) {
+    res.status(404).json({msg: "Produto não encontrado"});
+    return;
+  }
+
+  res.json(encontrado);
 });
 
+router.post("/produtos", function(req, res){
+  if (!req.body || !req.body.nome || !req.body.preco) {
+    res.status(422).json({msg: "Nome e/ou preco do produto obrigatorios"});
+    return;
+  }
+  const novo = {id:produtos.length+1, nome: req.body.nome, preco: req.body.preco};
+  produtos.push(novo);
+  res.status(201).json(novo);
+});
 
+router.put("/produtos/:produtoId", function(req, res){
+    const encontrado = produtos.find((produto) => produto.id == req.params.produtoId);
+  if (!encontrado) {
+    res.status(404).json({msg: "Produto não encontrado"});
+    return;
+  }
+  
+  encontrado.nome = req.body.nome;
+  encontrado.preco = req.body.preco;
+  res.json({});
+});
+
+router.delete("/produtos/:produtoId", function(req, res){
+    const posicao = produtos.findIndex((produto) => produto.id == req.params.produtoId);
+  if (posicao < 0) {
+    res.status(404).json({msg: "Produto não encontrado"});
+    return;
+  }
+
+  produtos.splice(posicao, 1);
+  res.status(204).end();
+});
+  
 
 module.exports = router;
